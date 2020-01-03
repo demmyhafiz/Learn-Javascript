dc30f181427198900c808669b69f77792970cbd7
demmyhafiz <hafiz.janitra@gmail.com>
2020-01-03 22:05:41 +0700

    add material from traversy media about for and while loops

--- main.js
+++ main.js
@@ -70,6 +70,7 @@ fruits.shift(); // to remove/delete/take the START of Arrays values
 */
 
 // Object Literals = key value pairs
+/*
 const person = {
 	firstName: 'John',
 	lastName: 'Doe',
@@ -95,20 +96,80 @@ console.log(firstName, city);
 person.email = 'john@gmail.com'; // add properties to an object
 
 console.log(person);
+*/
 
+const todos = [ // arrays of object (object in arrays)
+{
+	id: 1,
+	text: 'Take out trash',
+	isCompleted: true
+},
+{
+	id: 2,
+	text: 'Meeting with boss',
+	isCompleted: true
+},
+{
+	id: 3,
+	text: 'Dental appointment',
+	isCompleted: false
+}
+];
+/*
+console.log(todos);
+console.log(todos[1].text); // this is how to access object properties values in an arrays
 
+// JSON = data format , using API to send/receive to/from a server in JSON format
+const todoJSON = JSON.stringify(todos); // this is a method (stringify) convert object literals to JSON format data
+console.log(todoJSON);
+*/
 
+// For loops
+/*
+for(let i = 0; i < 10; i++) { // for loops start/print out from 0 (i = 0) then did an iteration (i++) then stop at 9 when the condition is false (because 10 is MUST i < 10)
+//	console.log(i);
+	console.log(`For loops number is ${i}`);
+}
+*/
 
+// While loops
+/*
+let i = 0; // the different from for loops is set the variable outside of the loops
+while(i < 10) {
+	console.log(`while loops number is ${i}`);
+	i++;
+}
+*/
+/*
+for(let i = 0; i < todos.length; i++) { // this is not the best way to loops through an array
+	console.log(todos[i].text); // i length aka todos length is 3, so it will print out all todos with properties text (in order of the index, 0,1,2, 3=false(i < 3)
+}
 
+for(let todo of todos) { // this is same as before with much better readable code
+	console.log(todo.text);
+}
+*/
+// High order array methods (suggest use it todo any kind of iteration/loops with arrays)
 
+// forEach (loops through inside of array), map (create new array from an array), filter (create new array based on a condition)
+/*
+todos.forEach(function(todo) { // forEach take in as parameter a function (function(), callback function takes in is todo, todo is a variable as each item in array
+	console.log(todo.text);
+});
+*/
+/*
+const todoText = todos.map(function(todo) { // loop through then return an array of just the text values
+	return todo.text; // takes the text values to the todoText variable and makes it an array
+});
 
+console.log(todoText);
+*/
 
+const todoCompleted = todos.filter(function(todo) {
+	return todo.isCompleted === true; // it's just takes the object that has properties true / the condition is true
+});
 
-
-
-
-
-
+console.log(todoCompleted);
 
 
 
